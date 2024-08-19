6c24dedc30d2098801874a436cc47642c0d67bf9
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-19 13:41:51 -0400

    Finished Project

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(200));
+
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year > 0 && year<= 9999) {
+            return div4Check(year);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div4Check(int year) {
+        if (year % 4 == 0) {
+            return div100Check(year);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div100Check(int year) {
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else {
+            return true;
+        }
+    }
 }
